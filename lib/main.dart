89d744f075774b1442cb909f72e524f4d6c6c65f
Matheus Silva <scruz.math@gmail.com>
2023-06-07 15:28:32 -0300

    Create

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/models/journal.dart';
+import 'package:flutter_webapi_first_course/screens/add_journal_screen/add_journal_screen.dart';
+import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'screens/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+
+  //JournalService service = JournalService();
+  //service.register(Journal.empty());
+  //service.getAll();
+
+  //asyncStudy();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple Journal',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      initialRoute: "home",
+      routes: {
+        "home": (context) => const HomeScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == "add-journal") {
+          Map<String, dynamic> map = settings.arguments as Map<String, dynamic>;
+          final Journal journal = map["journal"] as Journal;
+          final bool isEditing = map["is_editing"];
+          return MaterialPageRoute(builder: (context) {
+            return AddJournalScreen(journal: journal, isEditing: isEditing,);
+          });
+        }
+        return null;
+      },
+    );
+  }
+}
